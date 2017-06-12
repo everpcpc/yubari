e73698f1f668949c726153ee561ab9a6bdc455ae
everpcpc <git@everpcpc.com>
2017-06-12 21:39:16 +0800

    watch Strangestone for tawawa

--- yubari/bots/twitter_track.py
+++ yubari/bots/twitter_track.py
@@ -15,10 +15,14 @@ from yubari.lib.qq import qqbot
 
 logger = logging.getLogger(__name__)
 
-TWITTER_maesan = '2381595966'
-TWITTER_KanColle_STAFF = "294025417"
 KanColle_PROFILE_IMAGE_TMP = '/tmp/twitter_kancolle_staff_profile_image'
 kancolle_profile_image = ""
+
+TWITTERS = {
+    "KanColle_STAFF": "294025417",
+    "maesanpicture": "2381595966",
+    "Strangestone": "93332575"
+}
 
 
 def load_profile_img():
@@ -53,17 +57,17 @@ class MyStreamListener(StreamListener):
         if "event" in data:
             logger.debug("ignore event: %s", data["event"])
             return
-
         status = Status.parse(self.api, data)
-
         user = getattr(status, "user", None)
         if not user:
             logger.debug("empty user")
             return
-        if user.id_str == TWITTER_KanColle_STAFF:
+        if user.id_str == TWITTERS["KanColle_STAFF"]:
             self.proceed_kancolle(status)
-        elif user.id_str == TWITTER_maesan:
+        elif user.id_str == TWITTERS["maesanpicture"]:
             self.proceed_samidare(status)
+        elif user.id_str == TWITTERS["Strangestone"]:
+            self.proceed_tawawa(status)
         else:
             logger.debug("ignore user: %s", user.name)
 
@@ -71,13 +75,9 @@ class MyStreamListener(StreamListener):
         tags = status.entities.get("hashtags", [])
         if "艦これ" not in tags:
             return
-
         logger.info("kancolle: %s", status.text.replace("\n", " "))
         user = status.user
-
-        global kancolle_profile_image
         update_profile_img(user.profile_image_url_https)
-
         msg = [user.name]
         timestamp = int(int(status.timestamp_ms) / 1000)
         msg.append(datetime.fromtimestamp(timestamp).ctime())
@@ -88,11 +88,19 @@ class MyStreamListener(StreamListener):
         tags = status.entities.get("hashtags", [])
         if "毎日五月雨" not in tags:
             return
-
         logger.info("maesan: %s", status.text.replace("\n", " "))
         medias = status.entities.get("media", [])
         for media in medias:
             logger.info("samidare: %s", media["media_url_https"])
+            qqbot.sendSelfMsg(media["media_url_https"])
+
+    def proceed_tawawa(self, status):
+        if not status.text.startswith("月曜日のたわわ"):
+            return
+        logger.info("tawawa: %s", status.text.replace("\n", " "))
+        medias = status.entities.get("media", [])
+        for media in medias:
+            logger.info("tawawa: %s", media["media_url_https"])
             qqbot.sendSelfMsg(media["media_url_https"])
 
     def on_error(self, code):
@@ -103,7 +111,7 @@ def run():
     load_profile_img()
     msl = Stream(auth=ttapi.auth, listener=MyStreamListener())
     #  msl.filter(track=[TAG_SAMIDARE], follow=[TWITTER_KanColle_STAFF, TWITTER_maesan])
-    msl.filter(follow=[TWITTER_KanColle_STAFF, TWITTER_maesan])
+    msl.filter(follow=TWITTERS.values())
 
 
 if __name__ == '__main__':
