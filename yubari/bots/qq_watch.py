51cf3706504adb6b1772b491c6d9d612a64e49ab
everpcpc <git@everpcpc.com>
2017-06-18 01:02:51 +0800

    fix check mention

--- yubari/bots/qq_watch.py
+++ yubari/bots/qq_watch.py
@@ -4,11 +4,18 @@
 import time
 import logging
 
-from yubari.config import QQ_GROUP, MENTION_NAME
+from yubari.config import QQ_GROUP, MENTION_NAME, QQ_ME
 from yubari.lib.qq import qqbot
 
 
 logger = logging.getLogger(__name__)
+
+
+def check_mention_self(content):
+    for word in MENTION_NAME:
+        if word in content:
+            return True
+    return False
 
 
 def run():
@@ -18,16 +25,15 @@ def run():
     for msg in qqbot.poll():
         logger.info(msg)
         content = msg.get('msg').strip()
-        for word in MENTION_NAME:
-            if word in content:
-                now = int(time.time())
-                if now - last_call < 1800:
-                    logger.info("called in last 30min")
-                    return
-                call_msg = "呀呀呀，召唤一号机[CQ:at,qq=%s]" % QQ_ME
-                qqbot.sendGroupMsg(call_msg)
-                last_call = now
-                return
+        if check_mention_self(content):
+            now = int(time.time())
+            if now - last_call < 1800:
+                logger.info("called in last 30min")
+                continue
+            call_msg = "呀呀呀，召唤一号机[CQ:at,qq=%s]" % QQ_ME
+            qqbot.sendGroupMsg(call_msg)
+            last_call = now
+            continue
         if msg.get('event') == 'GroupMsg':
             if msg.get('group') == QQ_GROUP:
                 if content != last_msg:
