8b2856b7f3a7113284d0a1a1b7fb24476b36fa2f
everpcpc <git@everpcpc.com>
2017-07-29 19:45:57 +0800

    remove python ver

--- python_ver/yubari/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-import logging
-
-logging.basicConfig(level=logging.INFO,
-                    format='%(name)s - %(levelname)s - %(message)s')
-logging.getLogger('requests.packages.urllib3.connectionpool').setLevel(logging.WARN)
