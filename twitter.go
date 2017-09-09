b84574b8cd8e500771367bb74e1a7410056030c8
everpcpc <git@everpcpc.com>
2017-09-09 18:35:06 +0800

    add imascg_stage

--- twitter.go
+++ twitter.go
@@ -37,6 +37,7 @@ func NewTwitterBot(cfg *TwitterConfig) *TwitterBot {
 			"Strangestone":   "93332575",
 			"kazuharukina":   "28787294",
 			"sinoalice_jp":   "818752826025181184",
+			"imascg_stage":   "3220191374",
 		},
 	}
 	return bot
@@ -137,6 +138,12 @@ func (t *TwitterBot) trackTweet(tweet *twitter.Tweet) {
 		go checkSendKancolle(tweet, msg)
 
 	case t.Follows["sinoalice_jp"]:
+		logger.Infof("(%s):{%s} %d medias", tweet.User.Name, flattenedText, len(medias))
+		t := getTweetTime("Asia/Tokyo", tweet)
+		qqBot.SendGroupMsg(tweet.User.Name + "\n" + t + "\n\n" + msg)
+		sendPics(medias)
+
+	case t.Follows["imascg_stage"]:
 		logger.Infof("(%s):{%s} %d medias", tweet.User.Name, flattenedText, len(medias))
 		t := getTweetTime("Asia/Tokyo", tweet)
 		qqBot.SendGroupMsg(tweet.User.Name + "\n" + t + "\n\n" + msg)
