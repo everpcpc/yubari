a094e04a824dd6b3b6265effd5b3f8e90ab85ae4
everpcpc <git@everpcpc.com>
2020-03-23 10:58:40 +0800

    z

--- telegram/elasticsearch.go
+++ telegram/elasticsearch.go
@@ -1,10 +1,13 @@
 package telegram
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"strings"
 
 	elasticsearch7 "github.com/elastic/go-elasticsearch/v7"
+	"github.com/elastic/go-elasticsearch/v7/esapi"
 )
 
 var (
@@ -26,6 +29,12 @@ var (
 		}
 	}`
 )
+
+type esLog struct {
+	Content   string `json:"content"`
+	MessageID int    `json:"message_id"`
+	Date      int    `json:"date"`
+}
 
 func checkIndexExist(es *elasticsearch7.Client, idx string) (bool, error) {
 	res, err := es.Indices.Exists([]string{idx})
@@ -54,6 +63,28 @@ func createIndex(es *elasticsearch7.Client, idx string) error {
 
 	if res.IsError() {
 		return fmt.Errorf("create index %s error: %+v", idx, res)
+	}
+	return nil
+}
+
+func storeMessage(es *elasticsearch7.Client, idx string, message *esLog) error {
+	ret, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
+
+	req := esapi.IndexRequest{
+		Index:   idx,
+		Body:    strings.NewReader(string(ret)),
+		Refresh: "true",
+	}
+	res, err := req.Do(context.TODO(), es)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	if res.IsError() {
+		return fmt.Errorf("store message %s error: %+v", idx, res)
 	}
 	return nil
 }
